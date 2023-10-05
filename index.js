caac49ff45a358cbeda42365e156ca5da50b44b0
vamshichary7777 <119879631+vamshichary7777@users.noreply.github.com>
2023-10-05 15:17:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var express = require('express');
+const express = require('express');
+const app = express();
 
-var app = express();
-
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.SERVER_PORT || 3000; // Default port is 3000 if SERVER_PORT is not set
 
 app.get('/', function (req, res) {
   res.send('Simple Web Application is UP');
